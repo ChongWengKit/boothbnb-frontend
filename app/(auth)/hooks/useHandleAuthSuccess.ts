a070cbe56e1374eef6d64fd931172b1c11aceca4
chong wengkit <wengkitchong87@gmail.com>
2026-04-21 20:42:17 +0800

    testing vercel

--- app/(auth)/hooks/useHandleAuthSuccess.ts
+++ app/(auth)/hooks/useHandleAuthSuccess.ts
@@ -7,7 +7,7 @@ import toast from 'react-hot-toast';
 import type { User } from '@/app/contexts/UserContext';
 import { setAuthToken } from '@/app/contexts/auth';
 import { profile } from 'node:console';
-
+//test
 export const useHandleAuthSuccess = () => {
   const router = useRouter();
   const { setUser } = useUserContext();
