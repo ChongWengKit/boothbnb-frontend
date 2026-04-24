8156ad665e9cef9b63b42be53c42b992f5c08446
chong wengkit <wengkitchong87@gmail.com>
2026-04-24 16:28:41 +0800

    fix layout misalighment at auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,16 +9,18 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
         className="fixed inset-0 -z-10 bg-cover bg-center bg-no-repeat blur-md scale-105"
         style={{ backgroundImage: 'url(https://res.cloudinary.com/di3qccrxy/image/upload/v1775736549/photo-1497366216548-37526070297c_cvi9gq.avif)' }}
       />
-      <header className="px-4 md:px-8 py-6">
-        <Link href="/" className="text-xl font-bold text-white tracking-tight md:text-2xl">
-          BoothBnB
-        </Link>
-      </header>
-      <main className="p-4 flex items-center justify-center flex-1 m-8">
-        <div className="max-w-md">
-          {children}
-        </div>
-      </main>
+      <div className='h-[100dvh] flex flex-col'>
+        <header className="px-4 md:px-8 py-6">
+          <Link href="/" className="text-xl font-bold text-white tracking-tight md:text-2xl">
+            BoothBnB
+          </Link>
+        </header>
+        <main className="flex-col flex items-center justify-center flex-1">
+          <div className="max-w-md">
+            {children}
+          </div>
+        </main>
+      </div>
       <footer className="bg-card text-card-foreground text-center py-8 border-t border-border">
         Copyright &copy; {new Date().getFullYear()} BoothBnB
       </footer>
