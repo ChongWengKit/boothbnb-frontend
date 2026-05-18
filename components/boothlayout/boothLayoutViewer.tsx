5b48644ccb5ce37c377c9d52f644623fe9551b71
chong wengkit <wengkitchong87@gmail.com>
2026-05-18 13:46:47 +0800

    attempt to fix layout shifting

--- components/boothlayout/boothLayoutViewer.tsx
+++ components/boothlayout/boothLayoutViewer.tsx
@@ -46,12 +46,12 @@ export default function BoothLayoutViewer({
     };
 
     return (
-        <div className="mx-auto flex max-h-screen w-full max-w-lg flex-col items-center justify-center rounded-lg border border-border bg-card p-4 shadow-lg">
-            <div className="flex w-full justify-between border-b border-border pb-4">
+        <div className="mx-auto flex max-h-screen w-full max-w-lg flex-col rounded-lg border border-border bg-card shadow-lg">
+            <div className="flex w-full items-center justify-between border-b border-border p-4">
                 <h2 className="text-xl font-bold">Booth Layout</h2>
                 <IoClose size={24} onClick={onClose} className='cursor-pointer' />
             </div>
-            <div className="flex flex-col gap-4 overflow-y-scroll w-full p-4">
+            <div className="flex flex-col gap-4 overflow-y-scroll w-full p-4 flex-1">
                 <div className='flex flex-col gap-3 w-full'>
                     {booths.map((booth, index) => {
                         const isAvailable = booth.type === 'AVAILABLE';
